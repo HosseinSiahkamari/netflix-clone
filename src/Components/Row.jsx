375db61f512d59f1bface584da99c66e585fa401
Mohammadhossein <siahkamarii1363@gmail.com>
2023-09-13 22:48:40 +0200

    master

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -3,6 +3,9 @@ import axios from 'axios';
 import './Row.css';
 import Youtube from 'react-youtube';
 import movieTrailer from 'movie-trailer'
+import arrowLeft from '../assets/arrowLeft.png'
+import arrowRight from '../assets/arrowRight.png'
+
 
 
 
@@ -47,10 +50,16 @@ const Row = ({ title, fetchURL, isLarge }) => {
         <div className="row-container">
             <h2>{title} </h2>
             <div className="row-posters">
+                <button className="flash-button-left">
+                <img className='arrow' src={arrowLeft} alt="arrow" />
+                </button>
                 {movies.map(item => (
                     <img onClick={()=> handelClick(item) } className={`row-poster ${isLarge && "row-poster-large"}`} key={item.id} src={`https://image.tmdb.org/t/p/original${isLarge ? item.backdrop_path : item.poster_path}`} alt="item.poster_path" />
                 ))
                 }
+                <button className="flash-button-right">
+                <img className='arrow' src={arrowRight} alt="arrow" />
+                </button>
             </div>
             {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
 
