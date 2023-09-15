867081f1b8c4da27f5cb45217ec8375586bc855b
Mohammadhossein <siahkamarii1363@gmail.com>
2023-09-15 17:08:06 +0200

    master

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
     <div className="App">
       <Nav />
       <Banner fetchURL={requests.fetchNetflixOriginals} />
-      <Row title={'trending'} fetchURL={requests.fetchTranding} isLarge={true} />
-      <Row title={'Netflix Original'} fetchURL={requests.fetchNetflixOriginals} />
-      <Row title={'Top Rated'} fetchURL={requests.fetchTopRated} />
-      <Row title={'Action'} fetchURL={requests.fetchActionMovies} />
-      <Row title={'Comedy'} fetchURL={requests.fetchComedyMovies} />
-      <Row title={'Horror'} fetchURL={requests.fetchHorrorMovies} />
-      <Row title={'Romance'} fetchURL={requests.fetchRomanceMovies} />
-      <Row title={'Documentaries'} fetchURL={requests.fetchDocumentariesMovies} />
+      <Row rowId= '1' title={'trending'} fetchURL={requests.fetchTranding} isLarge={true} />
+      <Row rowId= '2' title={'Netflix Original'} fetchURL={requests.fetchNetflixOriginals} />
+      <Row rowId= '3' title={'Top Rated'} fetchURL={requests.fetchTopRated} />
+      <Row rowId= '4' title={'Action'} fetchURL={requests.fetchActionMovies} />
+      <Row rowId= '5' title={'Comedy'} fetchURL={requests.fetchComedyMovies} />
+      <Row rowId= '6' title={'Horror'} fetchURL={requests.fetchHorrorMovies} />
+      <Row rowId= '7' title={'Romance'} fetchURL={requests.fetchRomanceMovies} />
+      <Row rowId= '8' title={'Documentaries'} fetchURL={requests.fetchDocumentariesMovies} />
     </div>
   );
 }
--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -7,10 +7,7 @@ import arrowLeft from '../assets/arrowLeft.png'
 import arrowRight from '../assets/arrowRight.png'
 
 
-
-
-
-const Row = ({ title, fetchURL, isLarge }) => {
+const Row = ({ title, fetchURL, isLarge, rowId }) => {
 
     const [movies, setMovies] = useState([]);
     const [trailerUrl, setTrailerUrls] = useState("");
@@ -24,41 +21,88 @@ const Row = ({ title, fetchURL, isLarge }) => {
         apiRequest();
     }, [fetchURL]);
 
-    const opts = { 
-        heigth:'390',
-        width:'100%',
-        playersVars:{
+    const opts = {
+        height: '390',
+        width: '100%',
+        playersVars: {
             autoplay: 1,
         }
     }
-    const handelClick =(item)=>{
-        if(trailerUrl){
+    const handelClick = (item) => {
+        if (trailerUrl) {
             setTrailerUrls('')
-        }else{
+        } else {
             movieTrailer(item?.name || '')
-            .then((url) =>{
-                const urlParams =new URLSearchParams (new URL(url).search);
-                setTrailerUrls(urlParams.get("v"))
-            }
-               
-            ) .catch(error => console.log(error))
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrls(urlParams.get("v"))
+                }
+
+                ).catch(error => console.log(error))
         }
     }
+
+    const dynamicID = rowId;
+
+    const slideRightHandler = () => {
+        const rowSlideRight = document.getElementById(dynamicID);
+        const scrollAmount = 10; // میزان اسکرول به هر بار
+        const scrollSpeed = 1; // سرعت اسکرول
+        const targetScrollLeft = rowSlideRight.scrollLeft + 500; // مقدار مورد نظر برای اسکرول
+
+        const animateScroll = () => {
+            if (rowSlideRight.scrollLeft < targetScrollLeft) {
+                rowSlideRight.scrollLeft += scrollAmount;
+                const scrollLeft = rowSlideRight.scrollLeft;
+                requestAnimationFrame(() => {
+                    if (rowSlideRight.scrollLeft === scrollLeft) {
+                        animateScroll();
+                    }
+                });
+            }
+        };
+
+        animateScroll();
+    };
+
+
+    const slideLeftHandler = () => {
+        const rowSlideRight = document.getElementById(dynamicID);
+        const scrollAmount = 10; // میزان اسکرول به هر بار
+        const scrollSpeed = 1; // سرعت اسکرول
+        const targetScrollLeft = rowSlideRight.scrollLeft - 500; // مقدار مورد نظر برای اسکرول به سمت چپ
+
+        const animateScroll = () => {
+            if (rowSlideRight.scrollLeft > targetScrollLeft) {
+                rowSlideRight.scrollLeft -= scrollAmount;
+                const scrollLeft = rowSlideRight.scrollLeft;
+                requestAnimationFrame(() => {
+                    if (rowSlideRight.scrollLeft === scrollLeft) {
+                        animateScroll();
+                    }
+                });
+            }
+        };
+
+        animateScroll();
+    };
 
 
     return (
         <div className="row-container">
             <h2>{title} </h2>
             <div className="row-posters">
-                <button className="flash-button-left">
-                <img className='arrow' src={arrowLeft} alt="arrow" />
+                <button onClick={slideLeftHandler} className="flash-button-left">
+                    <img className='arrow' src={arrowLeft} alt="arrow" />
                 </button>
-                {movies.map(item => (
-                    <img onClick={()=> handelClick(item) } className={`row-poster ${isLarge && "row-poster-large"}`} key={item.id} src={`https://image.tmdb.org/t/p/original${isLarge ? item.backdrop_path : item.poster_path}`} alt="item.poster_path" />
-                ))
-                }
-                <button className="flash-button-right">
-                <img className='arrow' src={arrowRight} alt="arrow" />
+                <div id={dynamicID} className='row-slide'>
+                    {movies.map(item => (
+                        <img onClick={() => handelClick(item)} className={`row-poster ${isLarge && "row-poster-large"}`} key={item.id} src={`https://image.tmdb.org/t/p/original${isLarge ? item.backdrop_path : item.poster_path}`} alt="item.poster_path" />
+                    ))
+                    }
+                </div>
+                <button onClick={slideRightHandler} id='slideRight' className="flash-button-right">
+                    <img className='arrow' src={arrowRight} alt="arrow" />
                 </button>
             </div>
             {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
