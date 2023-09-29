c86519cae062585adca04d6db57f33c8fbca75ec
Mohammadhossein <siahkamarii1363@gmail.com>
2023-09-29 18:26:52 +0200

    respansive

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,68 @@
 // import './Nav.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import logo from '../../assets/image/logo.png'
 import search from '../../assets/image/search.png'
 import bell from '../../assets/image/bell.png'
+import DropDownMenu from '../DropDownMenu'
+import BellNotification from '../../BellNotification'
 
 const Nav = () => {
+
     const [show, setShow] = useState(false);
 
     const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 800);
     const [prevIsLargeScreen, setPrevIsLargeScreen] = useState(isLargeScreen);
+
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const [isMenuOpenOnMenu, setMenuOpenOnMenu] = useState(false);
+  
+    const [isMenuOpenBell, setMenuOpenBell] = useState(false);
+    const [isMenuOpenOnMenuBell, setMenuOpenOnMenuBell] = useState(false);
+
+
+
+    const toggleMenuEnter = () => {
+        setMenuOpen(true);
+        // setMenuOpenOnMenu(true)
+    }
+    const toggleMenuLeave = () => {
+        setTimeout(() => {
+            setMenuOpen(false) 
+        }, 500);
+    }
+    const handleMenuMouseEnter = () => {
+        // اگر ماوس روی منو قرار گرفته است، منو باز بماند
+        setMenuOpenOnMenu(true);
+    }
+
+    const handleMenuMouseLeave = () => {
+        // اگر ماوس از منو خارج شد، منو ببند
+        setTimeout(() => {
+            setMenuOpenOnMenu(false);
+        }, 500);
+    }
+    const toggleMenuEnterBell = () => {
+        setMenuOpenBell(true);
+        // setMenuOpenOnMenu(true)
+    }
+    const toggleMenuLeaveBell = () => {
+        setTimeout(() => {
+            setMenuOpenBell(false) 
+        }, 500);
+    }
+    const handleMenuMouseEnterBell = () => {
+        // اگر ماوس روی منو قرار گرفته است، منو باز بماند
+        setMenuOpenOnMenuBell(true);
+    }
+
+    const handleMenuMouseLeaveBell = () => {
+        // اگر ماوس از منو خارج شد، منو ببند
+        setTimeout(() => {
+            setMenuOpenOnMenuBell(false);
+        }, 500);
+    }
+    
+
 
     useEffect(() => {
         const handleResize = () => {
@@ -46,25 +100,34 @@ const Nav = () => {
                 <img src={logo} alt="" className="" />
             </div>
 
-            {isLargeScreen ? (<div className=" w-4/6 h-16 flex justify-between align-middle items-center">
-                <button className='text-xs md:text-xs lg:text-sm xl:text-md text-white ml-2' >Home</button>
-                <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >TV Shows</button>
-                <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >Movies</button>
-                <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >New & Papular</button>
-                <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >My List</button>
-                <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >Browse by Languages</button>
-            </div>) :
-                <div className='w-4/6 h-16 flex justify-between align-middle items-center'>
-                    <div className='flex'>
+            {isLargeScreen ? (
+                <div className=" w-4/6 h-16 flex justify-between align-middle items-center">
+                    <button className='text-xs md:text-xs lg:text-sm xl:text-md text-white ml-2' >Home</button>
+                    <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >TV Shows</button>
+                    <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >Movies</button>
+                    <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >New & Papular</button>
+                    <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >My List</button>
+                    <button className='text-xs md:text-xs lg:text-sm xl:text-sm text-white' >Browse by Languages</button>
+                </div>) :
+                <div className='w-4/6 h-16 flex justify-between align-middle items-center cursor-pointer'>
+                    <div className='flex' onMouseEnter={toggleMenuEnter} onMouseLeave={toggleMenuLeave} >
                         <p className='text-white pl-3 text-xs'>Browse</p>
                         <svg className='fill-white pl-2 pb-1' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z" /></svg>
                     </div>
+                    
 
                 </div>}
+                {isMenuOpen || isMenuOpenOnMenu ? (
+                    <DropDownMenu onMouseEnter={handleMenuMouseEnter} onMouseLeave={handleMenuMouseLeave} />
+                ) : ''}
+                
             <div className="flex justify-end w-2/5 h-full  items-center">
-                <div className=' w-16  items-center flex'><img className='w-full h-full px-4' src={search} alt="" /></div>
-                <div className=' w-16  items-center flex'><img className='h-3/4 px-4 ' src={bell} alt="" /></div>
+                <div className=' w-16  items-center flex cursor-pointer'><img className='w-full h-full px-4' src={search} alt="" /></div>
+                <div onMouseEnter={toggleMenuEnterBell} onMouseLeave={toggleMenuLeaveBell} className=' w-16  items-center flex cursor-pointer'><img className='h-3/4 px-4 ' src={bell} alt="bell" /></div>
             </div>
+            {isMenuOpenBell || isMenuOpenOnMenuBell ? (
+                <BellNotification onMouseEnterBell={handleMenuMouseEnterBell} onMouseLeaveBell={handleMenuMouseLeaveBell} />
+            ) : '' }
 
         </div>);
 }
--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import facebook from '../../assets/image/facebook.png'
 const Footer = () => {
     return (
         <div className="bg-zinc-900 w-full h-fit group">
-            <div className='flex pl-20'>
+            <div className='flex pl-10'>
                 <a href="https://www.facebook.com" target="_blank">
                     <img className='footer-icons' src={facebook} alt="footer-icons" />
                 </a>
@@ -21,7 +21,7 @@ const Footer = () => {
                     <img className='footer-icons' src={youtube} alt="footer-icons" />
                 </a>
             </div>
-            <ul className="grid grid-cols-2 h-fit md:grid-cols-4 w-3/4 py-5 ml-20">
+            <ul className="grid grid-cols-2 h-fit md:grid-cols-4 w-3/4 py-5 ml-10">
                 <li><a href="" className="link-text py-3 hover:underline">Audio Discription</a></li>
                 <li><a href="" className="link-text py-3 hover:underline">Help Center</a></li>
                 <li><a href="" className="link-text py-3 hover:underline">Gift Cards</a></li>
@@ -37,10 +37,10 @@ const Footer = () => {
                 <li><a href="" className="link-text py-3 hover:underline">Contact Us</a></li>
                 <li><a href="" className="link-text py-3 hover:underline">Ad Choices</a></li>
             </ul>
-            <button className="bg-zinc-900 cursor-pointer ml-20 w-26 h-8 border-solid border-2 border-zinc-400 text-zinc-400  rounded-sm p-1 ">Service Code</button>
+            <button className="bg-zinc-900 cursor-pointer ml-10 my-2 w-26 h-8 border-solid border-2 border-zinc-400 text-zinc-400  rounded-sm p-1 ">Service Code</button>
             <div className="w-3/5 h-12 px-2">
-                <p className='text-zinc-400 whitespace-nowrap ml-20 pt-2 text-xs'>2023 Netflix-Clone by M.Hossein Siahkamari</p>
-                <a href="https://github.com/HosseinSiahkamari/netflix-clone" target="_blank" className='text-zinc-400 whitespace-nowrap ml-20 text-xs' id='end-text'>https://github.com/HosseinSiahkamari/netflix-clone</a>
+                <p className='text-zinc-400 whitespace-nowrap ml-10 pt-2 text-xs'>2023 Netflix-Clone by M.Hossein Siahkamari</p>
+                <a href="https://github.com/HosseinSiahkamari/netflix-clone" target="_blank" className='text-zinc-400 whitespace-nowrap ml-10 text-xs' id='end-text'>https://github.com/HosseinSiahkamari/netflix-clone</a>
             </div>
         </div>);
 }
